c855fd3be8fa4bba54f50852892ffac9333d047d
Leltxu <isalguevar1@educacion.navarra.es>
2024-11-19 10:55:14 +0100

    Cracion de las tarjetas para los productos

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ $titulo="Pagina Principal";
 include "paginas/header.php";
 ?>
     <h1>Pagina principal</h1>
+
+    <div class="novedades">
+    <?php
+        for ($i=0; $i < 5; $i++) { 
+            include "plantillas/tarjeta.html";
+        }
+    ?>
+    </div>
 <?php
 include "paginas/footer.html";
 ?>
