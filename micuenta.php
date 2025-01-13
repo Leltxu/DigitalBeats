25fe9887c3b6c6bfe00cb0e535d64deb475553f0
GorkaMangado <gmangadlop@educacion.navarra.es>
2025-01-13 09:57:52 +0100

    crear micuenta.php y repetircontra.html

---
+++ micuenta.php
@@ -0,0 +1,18 @@
+<?php 
+include "header.php";
+$titulo="Mi cuenta";
+?>
+<body>
+<h3>Mi cuenta</h3>    
+
+<p>Nombre y Apellidos: Juan Garcia Medina</p>
+<p>Fecha de nacimiento: 27/08/1955</p>
+<p><a href="">Cambiar contraseña</a></p>
+<p>Direccion: C/Joaquin etorbidea, nº7, 4 izquierda</p>
+
+
+
+
+
+</body>
+</html>
