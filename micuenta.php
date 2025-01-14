53291a8d8432e8f2b8e9e94b7f326a8978be3bd5
GorkaMangado <gmangadlop@educacion.navarra.es>
2025-01-14 10:54:49 +0100

    Mi cuenta

--- micuenta.php
+++ micuenta.php
@@ -3,12 +3,21 @@ include "header.php";
 $titulo="Mi cuenta";
 ?>
 <body>
-<h3>Mi cuenta</h3>    
-
-<p>Nombre y Apellidos: Juan Garcia Medina</p>
-<p>Fecha de nacimiento: 27/08/1955</p>
-<p><a href="">Cambiar contraseña</a></p>
-<p>Direccion: C/Joaquin etorbidea, nº7, 4 izquierda</p>
+<h3>Mi cuenta</h3> 
+<div class="contenedor">
+    <div class="lateral">
+        <p>Pedidos anteriores</p>
+        <p><a href="cesta.php">Mi cesta</a></p>
+        <p>Mis opiniones</p>
+        <p><a href="cerrarsesion">Cerrar sesion</a></p>
+    </div> 
+    <div class="datos">
+        <p>Nombre y Apellidos: Juan Garcia Medina</p>
+        <p>Fecha de nacimiento: 27/08/1955</p>
+        <p><a href="">Cambiar contraseña</a></p>
+        <p>Direccion: C/Joaquin etorbidea, nº7, 4 izquierda</p>
+    </div>
+</div>
 
 
 
